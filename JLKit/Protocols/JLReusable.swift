75b0b68ef4d93abedf68871e44ed7f4d8ac64319
Woody <woody.jang@goodoc.co.kr>
2018-09-07 14:38:36 +0900

    no message

--- JLKit/Protocols/JLReusable.swift
+++ JLKit/Protocols/JLReusable.swift
@@ -11,13 +11,13 @@ import Foundation
 public protocol JLReusable {}
 
 extension JLReusable where Self: UIViewController {
-    internal static var reuseIdentifier: String {
+    public static var reuseIdentifier: String {
         return String(describing: self.self)
     }
 }
 
 extension JLReusable where Self: UIView {
-    internal static var reuseIdentifier: String {
+    public static var reuseIdentifier: String {
         return String(describing: self.self)
     }
 }
@@ -25,17 +25,17 @@ extension JLReusable where Self: UIView {
 public protocol JLNibLoadable { }
 
 extension JLNibLoadable where Self: UIViewController {
-    internal static func loadNib() -> Self? {
+    public static func loadNib() -> Self? {
         return Self(nibName: String(describing: self.self), bundle: nil)
     }
 }
 
 extension JLNibLoadable where Self: UIView {
-    internal static var nib: UINib? {
+    public static var nib: UINib? {
         return UINib(nibName: String(describing: self.self), bundle: nil)
     }
     
-    internal static func loadNib() -> Self? {
+    public static func loadNib() -> Self? {
         guard let views = Bundle.main.loadNibNamed(String(describing: self.self), owner: self, options: nil) else { return nil }
         for view in views {
             if let view = view as? Self {
@@ -47,32 +47,32 @@ extension JLNibLoadable where Self: UIView {
 }
 
 extension UITableView {
-    internal func register<T: UITableViewCell & JLReusable>(_: T.Type) {
+    public func register<T: UITableViewCell & JLReusable>(_: T.Type) {
         register(T.self, forCellReuseIdentifier: T.reuseIdentifier)
     }
     
-    internal func register<T: UITableViewCell & JLReusable & JLNibLoadable>(_: T.Type) {
+    public func register<T: UITableViewCell & JLReusable & JLNibLoadable>(_: T.Type) {
         guard let nib = T.nib else { return }
         register(nib, forCellReuseIdentifier: T.reuseIdentifier)
     }
     
-    internal func register<T: UITableViewHeaderFooterView & JLReusable>(_: T.Type) {
+    public func register<T: UITableViewHeaderFooterView & JLReusable>(_: T.Type) {
         register(T.self, forHeaderFooterViewReuseIdentifier: T.reuseIdentifier)
     }
     
-    internal func register<T: UITableViewHeaderFooterView & JLReusable & JLNibLoadable>(_: T.Type) {
+    public func register<T: UITableViewHeaderFooterView & JLReusable & JLNibLoadable>(_: T.Type) {
         guard let nib = T.nib else { return }
         register(nib, forHeaderFooterViewReuseIdentifier: T.reuseIdentifier)
     }
     
-    internal func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T where T: JLReusable {
+    public func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T where T: JLReusable {
         guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
             fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
         }
         return cell
     }
     
-    internal func dequeueReusableHeaderFooterView<T: UITableViewHeaderFooterView>() -> T? where T: JLReusable {
+    public func dequeueReusableHeaderFooterView<T: UITableViewHeaderFooterView>() -> T? where T: JLReusable {
         guard let view = dequeueReusableHeaderFooterView(withIdentifier: T.reuseIdentifier) as? T else {
             return nil
         }
@@ -81,32 +81,32 @@ extension UITableView {
 }
 
 extension UICollectionView {
-    internal func register<T: UICollectionReusableView & JLReusable>(_: T.Type, forSupplementaryViewOfKind elementKind: String) {
+    public func register<T: UICollectionReusableView & JLReusable>(_: T.Type, forSupplementaryViewOfKind elementKind: String) {
         register(T.self, forSupplementaryViewOfKind: elementKind, withReuseIdentifier: T.reuseIdentifier)
     }
     
-    internal func register<T: UICollectionReusableView & JLReusable & JLNibLoadable>(_: T.Type, forSupplementaryViewOfKind elementKind: String) {
+    public func register<T: UICollectionReusableView & JLReusable & JLNibLoadable>(_: T.Type, forSupplementaryViewOfKind elementKind: String) {
         guard let nib = T.nib else { return }
         register(nib, forSupplementaryViewOfKind: elementKind, withReuseIdentifier: T.reuseIdentifier)
     }
     
-    internal func register<T: UICollectionViewCell & JLReusable>(_: T.Type) {
+    public func register<T: UICollectionViewCell & JLReusable>(_: T.Type) {
         register(T.self, forCellWithReuseIdentifier: T.reuseIdentifier)
     }
     
-    internal func register<T: UICollectionViewCell & JLReusable & JLNibLoadable>(_: T.Type) {
+    public func register<T: UICollectionViewCell & JLReusable & JLNibLoadable>(_: T.Type) {
         guard let nib = T.nib else { return }
         register(nib, forCellWithReuseIdentifier: T.reuseIdentifier)
     }
     
-    internal func dequeueReusableCell<T: UICollectionViewCell>(forIndexPath indexPath: IndexPath) -> T where T: JLReusable {
+    public func dequeueReusableCell<T: UICollectionViewCell>(forIndexPath indexPath: IndexPath) -> T where T: JLReusable {
         guard let cell = dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
             fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
         }
         return cell
     }
     
-    internal func dequeueReusableSupplementaryView<T: UICollectionReusableView>(ofKind elementKind: String, for indexPath: IndexPath) -> T where T: JLReusable {
+    public func dequeueReusableSupplementaryView<T: UICollectionReusableView>(ofKind elementKind: String, for indexPath: IndexPath) -> T where T: JLReusable {
         guard let view = dequeueReusableSupplementaryView(ofKind: elementKind, withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
             fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
         }
