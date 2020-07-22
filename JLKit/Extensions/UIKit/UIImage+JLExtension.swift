72958db75179ca036266292e82bc9db1c140b56c
jangsy <jangsy@thedaybefore.me>
2020-07-22 09:17:00 +0900

    UIColor Tint에서 컬러에 알파값이 있을 경우 정상적으로 동작하지 않는 현상 수정

--- JLKit/Extensions/UIKit/UIImage+JLExtension.swift
+++ JLKit/Extensions/UIKit/UIImage+JLExtension.swift
@@ -43,27 +43,28 @@ extension UIImage {
     }
     
     public func withTint(_ color: UIColor) -> UIImage? {
+        #if os(iOS)
         if #available(iOS 13.0, *) {
             return withTintColor(color, renderingMode: .alwaysOriginal)
-        } else {
-            /*
-             let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-             UIGraphicsBeginImageContextWithOptions(size, false, scale)
-             color.set()
-             UIRectFill(rect)
-             draw(in: rect, blendMode: .destinationIn, alpha: 1.0)
-             let image = UIGraphicsGetImageFromCurrentImageContext()
-             UIGraphicsEndImageContext()
-             return image
-             */
-            defer { UIGraphicsEndImageContext() }
-            UIGraphicsBeginImageContextWithOptions(size, false, scale)
-            color.set()
-            self.withRenderingMode(.alwaysTemplate).draw(in: CGRect(origin: .zero, size: size))
-            return UIGraphicsGetImageFromCurrentImageContext()
         }
+        #endif
+        /*
+         let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+         UIGraphicsBeginImageContextWithOptions(size, false, scale)
+         color.set()
+         UIRectFill(rect)
+         draw(in: rect, blendMode: .destinationIn, alpha: 1.0)
+         let image = UIGraphicsGetImageFromCurrentImageContext()
+         UIGraphicsEndImageContext()
+         return image
+         */
+        defer { UIGraphicsEndImageContext() }
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        color.set()
+        self.withRenderingMode(.alwaysTemplate).draw(in: CGRect(origin: .zero, size: size))
+        return UIGraphicsGetImageFromCurrentImageContext()
     }
-    
+
     public func withOrientation(_ orientation: UIImage.Orientation) -> UIImage? {
         guard let cgImage = self.cgImage else { return nil }
         return UIImage(cgImage: cgImage, scale: scale, orientation: orientation)
