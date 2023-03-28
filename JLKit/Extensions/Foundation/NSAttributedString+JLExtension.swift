5cc6dde400483fb46463c17a6acedb294dba0565
장석용 <jangsy@thedaybefore.me>
2023-03-28 11:30:41 +0900

    신규 함수 추가

--- JLKit/Extensions/Foundation/NSAttributedString+JLExtension.swift
+++ JLKit/Extensions/Foundation/NSAttributedString+JLExtension.swift
@@ -12,6 +12,29 @@ public extension NSAttributedString {
     var isEmpty: Bool {
         return string.isEmpty
     }
+    
+    var attributes: [Key: Any] {
+        guard length > 0 else { return [:] }
+        return attributes(at: 0, effectiveRange: nil)
+    }
+}
+
+public extension NSAttributedString {
+    func fonted(with font: UIFont) -> NSAttributedString {
+        return applying(attributes: [.font: font])
+    }
+    
+    func colored(with color: UIColor) -> NSAttributedString {
+        return applying(attributes: [.foregroundColor: color])
+    }
+    
+    func applying(attributes: [Key: Any]) -> NSAttributedString {
+        guard !string.isEmpty else { return self }
+
+        let copy = NSMutableAttributedString(attributedString: self)
+        copy.addAttributes(attributes, range: NSRange(0..<length))
+        return copy
+    }
 }
 
 public extension NSMutableAttributedString {
@@ -25,3 +48,4 @@ public extension NSMutableAttributedString {
         append(NSAttributedString(string: text, attributes: attributes))
     }
 }
+
